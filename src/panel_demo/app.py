498e3b0aa6d540ae5e0e8c8c0a32cb56e2699158
Dan Birman <danbirman@gmail.com>
2024-12-04 12:28:40 -0800

    feat: basic interactivity with filtering

--- src/panel_demo/app.py
+++ src/panel_demo/app.py
@@ -1,7 +1,29 @@
 """Starter code to get your panel app running"""
 from panel_demo.utils import _get_metadata
+import pandas as pd
+import panel as pn
 
+
+pn.extension('vega')
 
 df = _get_metadata()
 
 print(df.head())
+
+# Filter based on a text input field
+
+text_input = pn.widgets.TextInput(name='Filter by name')
+
+
+def get_filtered_df(text):
+    filtered_df = df[df['name'].str.contains(text)]
+    return filtered_df
+
+
+def get_dataframe(value):
+    return pn.pane.DataFrame(get_filtered_df(value))
+
+
+col = pn.Column(text_input, pn.bind(get_dataframe, value=text_input))
+
+col.servable()
