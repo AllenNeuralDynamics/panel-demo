66946d1efca7b5fe6bf226a937dd1afb96d20dca
Dan Birman <danbirman@gmail.com>
2024-12-05 09:16:33 -0800

    feat: multi-filter version of app, with sync

---
+++ discussion.md
@@ -0,0 +1,9 @@
+To talk about:
+
+- Fast iteration
+    * imagining is harder than seeing
+    * how many steps from idea -> deployment
+- Tooling
+    * html/css/js
+    * js tools: react, etc
+    * 
--- src/panel_demo/app.py
+++ src/panel_demo/app.py
@@ -1,7 +1,44 @@
 """Starter code to get your panel app running"""
 from panel_demo.utils import _get_metadata
+import param
+import panel as pn
 
 
-df = _get_metadata()
+class Database(param.Parameterized):
+    name_filter = param.String(default="")
+    derived_filter = param.String(default="All")
 
-print(df.head())
+    def __init__(self):
+        super().__init__()
+        self.df = _get_metadata()
+
+    def filter_data(self):
+        df = self.df
+        if self.name_filter:
+            df = df[df["name"].str.contains(self.name_filter)]
+        if not self.derived_filter == "All":
+            df = df[df["derived"] == (self.derived_filter == "Derived")]
+        return df
+
+
+text_input = pn.widgets.TextInput(name="Filter by name:", value="")
+derived_input = pn.widgets.Select(name="Filter by name:", options=["All", "Derived", "Not Derived"])
+
+pn.state.location.sync(text_input, {"value": "name_filter"})
+pn.state.location.sync(derived_input, {"value": "name_filter"})
+
+db = Database()
+
+
+def build_df_pane(text_filter, derived_filter):
+    db.name_filter = text_filter
+    db.derived_filter = derived_filter
+    df = db.filter_data()
+    return pn.pane.DataFrame(df)
+
+
+df_pane = pn.bind(build_df_pane, text_input, derived_input)
+
+pane = pn.Column(text_input, derived_input, df_pane)
+
+pane.servable()
